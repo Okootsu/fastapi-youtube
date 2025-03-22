e43f69ffb6eb2639779d1938059bd54c2d384b0b
Okootsu <163650919+Okootsu@users.noreply.github.com>
2025-03-22 10:47:35 -0400

    Update main.py
    Actualizacion de la API

--- main.py
+++ main.py
@@ -12,29 +12,28 @@ def read_root():
 
 @app.post("/upload_cookies/")
 async def upload_cookies(cookies_file: UploadFile = File(...)):
-    # Crear un archivo temporal para las cookies
-    with tempfile.NamedTemporaryFile(delete=True) as temp_cookie_file:
-        temp_cookie_file.write(await cookies_file.read())
-        temp_cookie_file.flush()  # Asegurarse de que los datos estén escritos
-        # Retornar el nombre del archivo temporal o un ID único
-        return {"cookie_file": temp_cookie_file.name}
+    # Crear un directorio para almacenar cookies si no existe
+    os.makedirs("cookies", exist_ok=True)
+    
+    # Crear un archivo permanente para las cookies
+    file_path = os.path.join("cookies", cookies_file.filename)
+    with open(file_path, "wb") as f:
+        f.write(await cookies_file.read())
+    
+    return {"cookie_file": file_path}
 
 @app.get("/info_video/")
 def info_video(video_url: str):
-    # Opciones de configuración para obtener información sobre el video
     ydl_opts = {
-        'format': 'bestaudio[ext=m4a][abr<=128]'  # Filtrar por m4a y bitrate medio (<=128 kbps)
+        'format': 'bestaudio[ext=m4a][abr<=128]'
     }
 
-    # Obtener información del video
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        info_dict = ydl.extract_info(video_url, download=False)  # download=False para no descargar
+        info_dict = ydl.extract_info(video_url, download=False)
 
-    # Extraer información relevante
     title = info_dict.get('title', 'Título no disponible')
     author = info_dict.get('uploader', 'Autor no disponible')
         
-    # Retornar el título del video
     return {
         "titulo_video": title,
         "autor_video": author,
@@ -43,28 +42,21 @@ def info_video(video_url: str):
 @app.get("/download/")
 async def download_video(video_url: str, cookie_file: str):
     download_folder = "downloads/"
-    
-    # Asegurarse de que el directorio de descargas exista
     os.makedirs(download_folder, exist_ok=True)
 
-    # Opciones de configuración para filtrar y descargar formatos específicos
     ydl_opts = {
-        'format': 'bestaudio[ext=m4a][abr<=128]',  # Filtrar por m4a y bitrate medio (<=128 kbps)
-        'outtmpl': download_folder + '%(title)s.%(ext)s',  # Nombre del archivo como el título del video
-        'cookies': cookie_file  # Usar el archivo de cookies proporcionado
+        'format': 'bestaudio[ext=m4a][abr<=128]',
+        'outtmpl': download_folder + '%(title)s.%(ext)s',
+        'cookies': cookie_file
     }
 
-    # Intentar descargar el audio
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download([video_url])
     except Exception as e:
         raise HTTPException(status_code=500, detail="Error al descargar el video: " + str(e))
 
-    # Obtener información del video
-    info_dict = ydl.extract_info(video_url, download=False)  # download=False para no descargar
-
-    # Extraer información relevante
+    info_dict = ydl.extract_info(video_url, download=False)
     file_name = f"{info_dict.get('title')}.m4a"
     file_path = os.path.join(download_folder, file_name)
 
