6b81894dfc33695eabde728544bc70a803871c7b
Okootsu <163650919+Okootsu@users.noreply.github.com>
2025-03-22 10:00:54 -0400

    Update main.py
    Acutalizacion de la API

--- main.py
+++ main.py
@@ -10,6 +10,15 @@ app = FastAPI()
 def read_root():
     return {"message": "Bienvenido a la API de Descarga de Videos de Youtube"}
 
+@app.post("/upload_cookies/")
+async def upload_cookies(cookies_file: UploadFile = File(...)):
+    # Crear un archivo temporal para las cookies
+    with tempfile.NamedTemporaryFile(delete=True) as temp_cookie_file:
+        temp_cookie_file.write(await cookies_file.read())
+        temp_cookie_file.flush()  # Asegurarse de que los datos estén escritos
+        # Retornar el nombre del archivo temporal o un ID único
+        return {"cookie_file": temp_cookie_file.name}
+
 @app.get("/info_video/")
 def info_video(video_url: str):
     # Opciones de configuración para obtener información sobre el video
@@ -32,35 +41,25 @@ def info_video(video_url: str):
     }
 
 @app.get("/download/")
-async def download_video(video_url: str, cookies_file: UploadFile = File(None)):
+async def download_video(video_url: str, cookie_file: str):
     download_folder = "downloads/"
     
+    # Asegurarse de que el directorio de descargas exista
+    os.makedirs(download_folder, exist_ok=True)
+
     # Opciones de configuración para filtrar y descargar formatos específicos
     ydl_opts = {
         'format': 'bestaudio[ext=m4a][abr<=128]',  # Filtrar por m4a y bitrate medio (<=128 kbps)
         'outtmpl': download_folder + '%(title)s.%(ext)s',  # Nombre del archivo como el título del video
+        'cookies': cookie_file  # Usar el archivo de cookies proporcionado
     }
 
-    # Intentar descargar el audio sin cookies primero
+    # Intentar descargar el audio
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download([video_url])
     except Exception as e:
-        # Si falla, verificar si se proporcionó un archivo de cookies
-        if not cookies_file:
-            raise HTTPException(status_code=400, detail="Se requiere un archivo de cookies para la autenticación. Por favor, sube el archivo de cookies.")
-        
-        # Crear un archivo temporal para las cookies
-        with tempfile.NamedTemporaryFile(delete=True) as temp_cookie_file:
-            temp_cookie_file.write(await cookies_file.read())
-            temp_cookie_file.flush()  # Asegurarse de que los datos estén escritos
-
-            # Agregar el archivo temporal de cookies a las opciones
-            ydl_opts['cookies'] = temp_cookie_file.name
-            
-            # Intentar descargar nuevamente con las cookies
-            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                ydl.download([video_url])
+        raise HTTPException(status_code=500, detail="Error al descargar el video: " + str(e))
 
     # Obtener información del video
     info_dict = ydl.extract_info(video_url, download=False)  # download=False para no descargar
