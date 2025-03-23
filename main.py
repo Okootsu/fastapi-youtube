7e0ff49f0af8ec052e7e4be32bc7c1975de63908
Okootsu <163650919+Okootsu@users.noreply.github.com>
2025-03-23 15:03:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -16,28 +16,25 @@ def read_root():
 
 @app.post("/upload_cookies/")
 async def upload_cookies(cookies_file: UploadFile = File(...)):
-    global cookie_file  # Usar la variable global
-    # Leer el archivo de cookies y almacenarlo en un archivo temporal
+    global cookie_file
     cookie_storage = await cookies_file.read()
     
-    # Verificar que el archivo de cookies tenga el formato correcto
     if not cookie_storage.startswith(b"# HTTP Cookie File") and not cookie_storage.startswith(b"# Netscape HTTP Cookie File"):
         raise HTTPException(status_code=400, detail="El archivo de cookies no tiene el formato correcto.")
     
     temp_cookie_file = tempfile.NamedTemporaryFile(delete=False, suffix=".txt")
     temp_cookie_file.write(cookie_storage)
-    temp_cookie_file.close()  # Cerrar el archivo para que yt-dlp pueda leerlo
+    temp_cookie_file.close()
 
-    cookie_file = temp_cookie_file.name  # Guardar la ruta del archivo temporal
+    cookie_file = temp_cookie_file.name
+    print(f"Archivo de cookies guardado en: {cookie_file}")  # Mensaje de depuración
 
-    return {"cookie_file": cookie_file}  # Retorna la ruta del archivo temporal
-
+    return {"cookie_file": cookie_file}
 
 @app.get("/download/")
 async def download_video(video_url: str):
-    global cookie_file  # Usar la variable global
+    global cookie_file
 
-    # Verifica que el archivo de cookies existe
     if not cookie_file or not os.path.exists(cookie_file):
         raise HTTPException(status_code=400, detail="No se han subido cookies")
 
@@ -47,24 +44,23 @@ async def download_video(video_url: str):
     ydl_opts = {
         'format': 'bestaudio[ext=m4a][abr<=128]',
         'outtmpl': download_folder + '%(title)s.%(ext)s',
-        'cookies': cookie_file,  # Usar el archivo temporal de cookies
+        'cookies': cookie_file,
         'http_headers': {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36',
-    },
-        'verbose': True,  # Para obtener más detalles sobre el proceso
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36',
+        },
+        'verbose': True,
     }
 
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download([video_url])
     except Exception as e:
+        print(f"Error al descargar el video: {str(e)}")  # Mensaje de depuración
         raise HTTPException(status_code=500, detail="Error al descargar el video: " + str(e))
     finally:
-        # Limpia el archivo de cookies después de usarlo
         if os.path.exists(cookie_file):
             os.remove(cookie_file)
 
-    # Obtener información del video
     info_dict = ydl.extract_info(video_url, download=False)
     file_name = f"{info_dict.get('title')}.m4a"
     file_path = os.path.join(download_folder, file_name)
